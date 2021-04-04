619099d31bd2133176a3efad1fb3e38c0524b022
varCODEx <borys.kovba@gmail.com>
2021-04-05 01:13:46 +0300

    RB test

--- tree_test.go
+++ tree_test.go
@@ -30,9 +30,33 @@ func TestRBTree(t *testing.T) {
 	tree := NewRedBlackTree(&element{50})
 	fmt.Println(tree)
 
-	fmt.Println(tree.find(&element{50}))
-	fmt.Println(tree.find(&element{500}))
-	fmt.Println(tree.find(&element{5000}))
+	tree.insert(&element{5})
+	fmt.Println(tree)
 
+	tree.insert(&element{10})
+	fmt.Println(tree)
+
+	tree.insert(&element{15})
+	fmt.Println(tree)
+
+	tree.insert(&element{7})
+	fmt.Println(tree)
+
+	tree.insert(&element{4})
+	fmt.Println(tree)
+
+	tree.insert(&element{2})
+	fmt.Println(tree)
+
+	tree.insert(&element{8})
+	fmt.Println(tree)
+
+	tree.insert(&element{7})
+	fmt.Println(tree)
+
+	tree.insert(&element{222})
+	fmt.Println(tree)
+
+	tree.insert(&element{0})
 	fmt.Println(tree)
 }
